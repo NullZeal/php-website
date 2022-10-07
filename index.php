327ea67507815d41867236de675f215470ff569b
NullZeal <nullzeal@pm.me>
2022-10-07 14:05:44 -0400

    Refactored the generatePageHead function to make it clearer Added text to the index page

--- index.php
+++ index.php
@@ -15,7 +15,18 @@ generatePageHead($pageTitle, $cssHomePage)
 ?>
 
 <body>
-    <img id="logo" src="<?php echo $pictureLogo; ?>" alt="alt" />
+    <div>
+        <img id="logo" src="<?php echo $pictureLogo; ?>" alt="alt" />
+        <h1>Pontbriand inc.</h1>
+        <h2>Privacy at it's best. Because everybody has something to hide. And that's perfectly fine.</h2>
+        <p>At Pontbriand inc., we believe in a free and secure Internet. We offer the world open source technologies to compete agains't the products offered by companies that do not respect your rights.</p>
+        
+        
+    </div>
+    
+    
+    
+    
     
 </body>
 
--- php/globalFunctions.php
+++ php/globalFunctions.php
@@ -14,33 +14,34 @@ define("FILE_GLOBAL_CSS", FOLDER_CSS . "global.css");
 $pictureLogo = "pictures/logo.png";
 
 //The function generatePageHead() will take both a title and a css argument, defined in their respective php pages, to ensure a dynamic approach. This function is created to generate the html boilerplates dynamically.
-function generatePageHead($title, $cssFile){
+function generatePageHead($title, $cssFile) {
     ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta title="Home Page">
-        <title><?php echo $title; ?></title>
-        <?php 
-        //Below I added the reset and global files as a constant with the define in the top of the page to show that I can also use constants to reference a file.
-        ?> 
-        <link rel="stylesheet" type="text/css" href="<?php echo FILE_RESET_CSS; ?>">
-        <link rel="stylesheet" type="text/css" href="<?php echo FILE_GLOBAL_CSS; ?>">
-        <?php 
-        //Below I added the page specific css file last with the variable taken from the function parameter
-        ?> 
-         <link rel="stylesheet" type="text/css" href="<?php echo $cssFile; ?>">
-    </head>
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta title="Home Page">
+            <title><?php echo $title; ?></title>
 
-<?php
-}
+            <?php //Below I added the reset and global files as a constant with the define in the top of the page to show that I can also use constants to reference a file.?> 
+            <link rel="stylesheet" type="text/css" href="
+                  <?php echo FILE_RESET_CSS; ?>">
+            <link rel="stylesheet" type="text/css" href="
+                  <?php echo FILE_GLOBAL_CSS; ?>">
 
-function generatePageFooter(){
-    ?>
-    <footer>
-        Copyright Julien Pontbriand (202135020) 2022.        
-    </footer>
-    </html>
+            <?php //Below I added the page specific css file last with the variable taken from the function parameter?> 
+            <link rel="stylesheet" type="text/css" href="
+                  <?php echo $cssFile; ?>">
+        </head>
+
         <?php
+    }
+
+    function generatePageFooter() {
+        ?>
+        <footer>
+            Copyright Julien Pontbriand (202135020) 2022.        
+        </footer>
+    </html>
+    <?php
 }
