9bdc44aea3c16ae0625fefb7614d8aa1f7c2ebf2
NullZeal <nullzeal@pm.me>
2022-10-07 11:17:55 -0400

    Removed > from the copyright signature

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
 ?>
-Copyright Julien Pontbriand (202135020)> 2022.
+Copyright Julien Pontbriand (202135020) 2022.
