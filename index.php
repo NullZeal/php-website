cdc5c809ecb2de99bcd5236cd5567386498ecdb1
NullZeal <nullzeal@pm.me>
2022-10-07 12:20:11 -0400

    Added comments Added a globalFunctions reference variable Added a pageTitle variable Added the generatePageHead function with required variables

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
+//Importing the global functions from the relative path given in $globalFunctions
+$globalFunctions = 'php/globalFunctions.php';
+require_once $globalFunctions;
 
+//Defining the title for this page
+$pageTitle = "Home Page";
 
-
-
+//Generating the page head with the title and css references 
+generatePageHead($pageTitle, $cssHomePage)
 
 ?>
 Copyright Julien Pontbriand (202135020) 2022.
