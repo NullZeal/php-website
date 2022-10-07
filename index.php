e00bdcdc262dc7efa6ef53a9e83f076db4349e12
Julien Pontbriand (202135020) <>
2022-10-07 14:50:26 -0400

    Added the project pdf for instructions to the files of the project Added a background color for the index page Added an alt to the logo image in the index php file

--- index.php
+++ index.php
@@ -12,15 +12,15 @@ $pageTitle = "Home Page";
 //Generating the page head with the title and css references 
 generatePageHead($pageTitle, $cssHomePage)
 
+
 ?>
 
 <body>
     <div>
-        <img id="logo" src="<?php echo $pictureLogo; ?>" alt="alt" />
+        <img id="logo" src="<?php echo $pictureLogo; ?>" alt="logo of Julien Pontbriand inc." />
         <h1>Pontbriand inc.</h1>
         <h2>Privacy at it's best. Because everybody has something to hide. And that's perfectly fine.</h2>
         <p>At Pontbriand inc., we believe in a free and secure Internet. We offer the world open source technologies to compete agains't the products offered by companies that do not respect your rights.</p>
-        
         
     </div>
     
