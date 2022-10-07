2357c2c9de157d96e0cc15f70e3301c49e964807
NullZeal <nullzeal@pm.me>
2022-10-07 11:15:34 -0400

    Added several folders to the project (php, js, css and pictures)
    Added 2 css files, one for the index and one for the reset (I will make sure the default css proprieties are the same on all browsers by adding default css proprieties taken from the web)
    
    Added a copyright line at the end of the index.php file

--- index.php
+++ index.php
@@ -4,4 +4,5 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
  */
-
+?>
+Copyright Julien Pontbriand (202135020)> 2022.
