6ecb5090f17aef6ae5804884a991d6d8132e4175
NullZeal <nullzeal@pm.me>
2022-10-07 11:38:06 -0400

    Created 3 variables, all of which point directly to a specific css file belonging to a page
    Created a working, yet unfinished function to create the page top

--- php/globalFunctions.php
+++ php/globalFunctions.php
@@ -1,2 +1,19 @@
 <?php
+//Here I decided to start the name of the variables as 'css' to facilitate finding their references in the page php files
+$cssIndex = "css/index.css";
+$cssBuying = "css/buying.css";
+$cssOrders = "css/orders.csss";
 
+function pageTop($title, $cssFile){
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta title="Home Page">
+    </head>
+
+
+<?php
+}
