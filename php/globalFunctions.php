00a882c14988b29415b745f8cb40a9b35c0e5ce3
NullZeal <nullzeal@pm.me>
2022-10-07 12:04:42 -0400

    Changed the function pageTop name to generatePageHead (corresponds better to what it does)
    Modified comments of the globalFunctions file
    
    Refactored the generatePageHead function

--- php/globalFunctions.php
+++ php/globalFunctions.php
@@ -1,6 +1,5 @@
 <?php
-//The name of the following variables start as 'css' to facilitate finding 
-//their references in the page php files
+//The name of the following variables start as 'css' to facilitate finding their references in the page php files
 $cssIndex = "css/index.css";
 $cssBuying = "css/buying.css";
 $cssOrders = "css/orders.csss";
@@ -9,8 +8,8 @@ $cssReset = "css/reset.css";
 define("FOLDER_CSS", "css/");
 define("FILE_RESET_CSS", FOLDER_CSS . "reset.css");
 
-//The function pageTop() function will take both a title and a css argument, defined in their respective php pages, to ensure a dynamic approach. This function is created to generate the html boilerplates dynamically.
-function pageTop($title, $cssFile){
+//The function generatePageHead() will take both a title and a css argument, defined in their respective php pages, to ensure a dynamic approach. This function is created to generate the html boilerplates dynamically.
+function generatePageHead($title, $cssFile){
     ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,11 +19,9 @@ function pageTop($title, $cssFile){
         <title><?php echo $title; ?></title>
         <link rel="stylesheet" type="text/css" href="<?php echo $cssFile; ?>">
         <?php 
-        // Below I added the reset file as a constant define in the page top to show that I ca           n also use constants to reference a file
+        //Below I added the reset file as a constant with the define in the top of the page            to show that I can also use constants to reference a file.
         ?> 
         <link rel="stylesheet" type="text/css" href="<?php echo FILE_RESET_CSS; ?>">
     </head>
-
-
 <?php
 }
