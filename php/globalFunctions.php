5c9e3fce202e41edcdd5eac0e34c91a29569aaa3
Julien Pontbriand (202135020) <>
2022-10-22 14:56:56 -0400

    (Sorry about last commmit, it did not incluse all file changes) (Last commit used the wrong default Author name as well)
    Refactored in the HTML appearance in the page source
    Added new functions to generate HTML tags
    Modified index page content
    Modified css files

--- php/globalFunctions.php
+++ php/globalFunctions.php
@@ -1,54 +1,98 @@
 <?php
-//The name of the following variables start as 'css' to facilitate finding their references in the page php files
-$cssHomePage = "css/index.css";
-$cssBuying = "css/buying.css";
-$cssOrders = "css/orders.csss";
-$cssReset = "css/reset.css";
 
-//Creating a constant for olders
+//Creating a constant for folders
 define("FOLDER_CSS", "css/");
 define("FOLDER_PICTURES", "pictures/");
+define("FOLDER_MAIN", "./");
 
-//Creating a constant for iles
-define("FILE_CSS_RESET", FOLDER_CSS . "reset.css");
+//Creating a constant for files
+define("FILE_CSS_INDEX", FOLDER_CSS . "index.css");
+define("FILE_CSS_BUYING", FOLDER_CSS . "buying.css");
+define("FILE_CSS_ORDERS", FOLDER_CSS . "orders.css");
 define("FILE_CSS_GLOBAL", FOLDER_CSS . "global.css");
+
+define("FILE_PHPPAGE_HOME", FOLDER_MAIN . "index.php");
+define("FILE_PHPPAGE_BUYING", FOLDER_MAIN . "buying.php");
+define("FILE_PHPPAGE_ORDERS", FOLDER_MAIN . "orders.php");
+
 define("FILE_PICTURES_LOGO", FOLDER_PICTURES . "logo.png");
 
 //Creating a variable for the logo of the company
 $pictureLogo = "pictures/logo.png";
 
 //The function generatePageHead() will take both a title and a css argument, defined in their respective php pages, to ensure a dynamic approach. This function is created to generate the html boilerplates dynamically.
+
+function openDoctypeTag(){
+    ?><!DOCTYPE html><?php
+}
+
+function openHtmlTag(){
+    ?>
+
+<html lang="en"><?php
+}
+
 function generatePageHead($title, $cssFile)
 {
 
-    ?><!DOCTYPE html>
-                <html lang="en">
-                    <head>
-                        <meta charset="UTF-8">
-                        <meta title="Home Page">
-                        <title><?php echo $title; ?></title>
-            <?php //Below I added the reset and global files as a constant with the define in the top of the page to show that I can also use constants to reference a file.  ?> 
-                        <link rel="stylesheet" type="text/css" href="<?php echo FILE_CSS_RESET; ?>">
-                        <link rel="stylesheet" type="text/css" href="<?php echo FILE_CSS_GLOBAL; ?>">
-            <?php //Below I added the page specific css file last with the variable taken from the function parameter  ?> 
-                        <link rel="stylesheet" type="text/css" href="<?php echo $cssFile; ?>">
-                    </head>
-                  <?php
-              }
-
-              function generatePageFooter()
-{
-    ?>
-            <footer>
-            Copyright Julien Pontbriand (202135020) 2022.        
-            </footer>
-        </html>
-    <?php
+?>  
+    <head>
+        <meta charset="UTF-8">
+        <meta title="Home Page"><title><?php echo $title; ?></title>
+<?php //Below I added  the global CSS file as a constant with the define in the top of the page to show that I can use constants to reference a file.  ?>
+        <link rel="stylesheet" type="text/css" href="<?php echo FILE_CSS_GLOBAL; ?>">
+<?php //Below I added the page specific css file last with the variable taken from the function parameter  ?>
+        <link rel="stylesheet" type="text/css" href="<?php echo $cssFile; ?>">
+    </head>
+<?php
 }
+
+function openBodyTag(){
+    ?>
+    
+    <body><?php
+}
+
+function closeBodyTag(){
+    ?></body><?php
+}
+
+function closeHtmlTag(){
+    ?></html><?php
+}
+
 
 function generateNavigationMenu()
 {
+
+    ?>
+        
+        <div class="navigation">
+            <a href="<?php echo FILE_PHPPAGE_HOME?>">Home</a>
+            <a href="<?php echo FILE_PHPPAGE_BUYING?>">Buying</a>
+            <a href="<?php echo FILE_PHPPAGE_ORDERS?>">Orders</a>
+        </div>
+
+    <?php
+}
+
+
+
+
+
+
+
+
+
+
+function generatePageFooter()
+{
+    ?>
+    <footer>
+            Copyright Julien Pontbriand (202135020) <?php echo date("Y") ?>.
+        </footer>
     
+    <?php
 }
 
 function generateLogo()
@@ -59,34 +103,64 @@ function generateLogo()
     <?php
 }
 
-function generateIndexPage()
-{
-
-    ?>
-    <body>
-        <div>
-            <?php generateLogo(); ?>
-            <h1>Pontbriand inc.</h1>
-            <h2>Privacy at it's best. Because everybody has something to hide. And that's perfectly fine.</h2>
-            <p>At Pontbriand inc., we believe in a free and secure Internet.
-                We offer the world open source technologies tocompete agains't the products offered by companies that do not respect your rights.</p>
-
-        </div>
-        <section>
-
-        </section>
-
-    </body>
-    <?php
-}
 
 
 function addCachingPreventionHeaders(){
-    header("Expires: Tue, 29, Nov 1994 13:00 GMT");
+    header("Expires: Tue, 29, Nov 2024 13:00 GMT");
     header("Cache-Control: no-cache");
     header("Pragma: no-cache");
 }
 
 function addContentTypeHeader(){
     header('Content-type: text/html; charset=UTF-8');
+}
+
+
+
+
+
+
+
+
+
+
+function addErrorHandling(){
+    error_reporting(E_ALL);
+    set_error_handler("manageError");
+    set_exception_handler("manageException");
+}
+
+#CONSTANTS
+define("DEBUGGING", true);
+
+function manageError($errorNumber, $errorString, $errorFile, $errorLineNumber){
+    echo "an error occured on the line $errorNumber in the file $errorFile: " 
+            . "errorString ($errorNumber)";
+    #save detailed error in the file
+    die();
+
+}
+
+function manageException($errorObject)
+{
+    if(DEBUGGING){
+        #detailled ERROR
+     echo "An exception occured on the line "
+    . $errorObject->getLine() 
+    . " of the file "
+    . $errorObject->getMessage() 
+    . "(" 
+    . $errorObject->getCode()
+    . ")";
+    }
+    
+    else {
+        echo "An error has occured!";
+    }
+    
+    #save detailed error in the file
+    #file_put_contents() 
+    #ADD DATE TO THE LOGS
+    
+    die();
 }
