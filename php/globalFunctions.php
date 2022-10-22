73bcf1544ed371fbdd0715a724ce97f378fc9463
Julien Pontbriand (202135020) <>
2022-10-22 11:54:39 -0400

    Refactored the globalFunctions file Added new functions to generate headers

--- php/globalFunctions.php
+++ php/globalFunctions.php
@@ -18,44 +18,75 @@ define("FILE_PICTURES_LOGO", FOLDER_PICTURES . "logo.png");
 $pictureLogo = "pictures/logo.png";
 
 //The function generatePageHead() will take both a title and a css argument, defined in their respective php pages, to ensure a dynamic approach. This function is created to generate the html boilerplates dynamically.
-function generatePageHead($title, $cssFile) {
+function generatePageHead($title, $cssFile)
+{
+
+    ?><!DOCTYPE html>
+                <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta title="Home Page">
+                        <title><?php echo $title; ?></title>
+            <?php //Below I added the reset and global files as a constant with the define in the top of the page to show that I can also use constants to reference a file.  ?> 
+                        <link rel="stylesheet" type="text/css" href="<?php echo FILE_CSS_RESET; ?>">
+                        <link rel="stylesheet" type="text/css" href="<?php echo FILE_CSS_GLOBAL; ?>">
+            <?php //Below I added the page specific css file last with the variable taken from the function parameter  ?> 
+                        <link rel="stylesheet" type="text/css" href="<?php echo $cssFile; ?>">
+                    </head>
+                  <?php
+              }
+
+              function generatePageFooter()
+{
     ?>
-    <!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta title="Home Page">
-            <title><?php echo $title; ?></title>
-
-            <?php //Below I added the reset and global files as a constant with the define in the top of the page to show that I can also use constants to reference a file. ?> 
-            <link rel="stylesheet" type="text/css" href="
-                  <?php echo FILE_CSS_RESET; ?>">
-            <link rel="stylesheet" type="text/css" href="
-                  <?php echo FILE_CSS_GLOBAL; ?>">
-
-            <?php //Below I added the page specific css file last with the variable taken from the function parameter ?> 
-            <link rel="stylesheet" type="text/css" href="
-                  <?php echo $cssFile; ?>">
-        </head>
-
-        <?php
-    }
-
-    function generatePageFooter() {
-        ?>
-        <footer>
+            <footer>
             Copyright Julien Pontbriand (202135020) 2022.        
-        </footer>
-    </html>
+            </footer>
+        </html>
     <?php
 }
 
-function generateNavigationMenu() {
+function generateNavigationMenu()
+{
     
 }
 
-function generateLogo() {
+function generateLogo()
+{
+
     ?>
-        <img id="logo" src="<?php echo FILE_PICTURES_LOGO; ?>" alt="logo of Julien Pontbriand inc." />
+    <img id="logo" src="<?php echo FILE_PICTURES_LOGO; ?>" alt="logo of Julien Pontbriand inc." />
     <?php
+}
+
+function generateIndexPage()
+{
+
+    ?>
+    <body>
+        <div>
+            <?php generateLogo(); ?>
+            <h1>Pontbriand inc.</h1>
+            <h2>Privacy at it's best. Because everybody has something to hide. And that's perfectly fine.</h2>
+            <p>At Pontbriand inc., we believe in a free and secure Internet.
+                We offer the world open source technologies tocompete agains't the products offered by companies that do not respect your rights.</p>
+
+        </div>
+        <section>
+
+        </section>
+
+    </body>
+    <?php
+}
+
+
+function addCachingPreventionHeaders(){
+    header("Expires: Tue, 29, Nov 1994 13:00 GMT");
+    header("Cache-Control: no-cache");
+    header("Pragma: no-cache");
+}
+
+function addContentTypeHeader(){
+    header('Content-type: text/html; charset=UTF-8');
 }
